04ca19646e62733e4959d5d9975a6ec013f5cc0c
ChrisKim <1515748548@qq.com>
2023-08-22 17:20:37 +0800

    图片链接提取到配置文件

--- src/Components/CtrlPanel.jsx
+++ src/Components/CtrlPanel.jsx
@@ -36,7 +36,7 @@ class CtrlPanel extends Component {
     }
     args += `&size=${encodeURIComponent(this.state.size)}`;
     args += `&r18=${this.state.r18 ? "1" : "0"}`;
-    args += "&proxy=https://nahida.cc/pximg/"
+    args += `&proxy=${window.Config.imgURL}`
     return args.substring(1);
   }
 
